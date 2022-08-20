75d27338c3e50bdc25fad0c4f7504e582f93d45a
Markus Nyman <nymak@tuta.io>
2022-08-21 02:58:26 +0300

    background to TypeScript

--- src/background/api.ts
+++ src/background/api.ts
@@ -2,13 +2,13 @@ import ky from 'ky'
 
 const BASE_URL = 'https://api.faceit-enhancer.com'
 
-const api = ky.extend({ prefixUrl: BASE_URL })
+const api: typeof ky = ky.extend({ prefixUrl: BASE_URL })
 
 const BANS = atob('YmFucw==')
 
-export const fetchBan = guid => api(`${BANS}?guid=${guid}`).json()
+export const fetchBan = (guid: string) => api(`${BANS}?guid=${guid}`).json()
 
-export const fetchVips = guids =>
+export const fetchVips = (guids: string[]) =>
   api(`vips?guid=${Array.isArray(guids) ? guids.join('&guid=') : guids}`).json()
 
 export default api
